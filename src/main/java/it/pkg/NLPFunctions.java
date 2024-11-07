203e91e1fd8de68bb791ec631a07b5e973d422a7
Wasee Alam <waseealam@protonmail.com>
2024-11-07 11:35:29 -0500

    fixed formatting

--- src/main/java/it/pkg/NLPFunctions.java
+++ src/main/java/it/pkg/NLPFunctions.java
@@ -18,12 +18,14 @@ import io.trino.spi.function.Description;
 import io.trino.spi.function.ScalarFunction;
 import io.trino.spi.function.SqlType;
 
-public class NLPFunctions {
+public class NLPFunctions 
+{
 
     @ScalarFunction("square")
     @Description("Returns the square of a number")
     @SqlType("double")
-    public static double square(@SqlType("double") double value) {
+    public static double square(@SqlType("double") double value) 
+    {
         return value * value;
     }
 }
--- src/main/java/it/pkg/NLPPlugin.java
+++ src/main/java/it/pkg/NLPPlugin.java
@@ -15,15 +15,13 @@
 package it.pkg;
 
 import java.util.Set;
-
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-
 import io.trino.spi.Plugin;
 import io.trino.spi.connector.ConnectorFactory;
 
-public class NLPPlugin implements Plugin {
-
+public class NLPPlugin implements Plugin 
+{
     @Override
     public Iterable<ConnectorFactory> getConnectorFactories()
     {
